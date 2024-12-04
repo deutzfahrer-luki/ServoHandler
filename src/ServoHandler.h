4a4459cf6e42ace1f2027ce1851f7de07fe32eb3
deutzfahrer-luki <traktor.koeppl@gmail.com>
2024-12-04 15:06:48 +0100

    init logik files for the lib

---
+++ src/ServoHandler.h
@@ -0,0 +1,14 @@
+// EEPROMHandler.h
+#ifndef SERVOHANDLER_H
+#define SERVOHANDLER_H
+
+#include <Arduino.h>
+
+class ServoHandler
+{
+public:
+    ServoHandler(){}
+private:
+};
+
+#endif // SERVOHANDLER_H
