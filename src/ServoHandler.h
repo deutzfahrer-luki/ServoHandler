959df109cdccfe37a18d630de41849008c71d580
Lukas <traktor.koeppl@gmail.com>
2024-12-05 08:09:31 +0100

    update the files ka

--- src/ServoHandler.h
+++ src/ServoHandler.h
@@ -3,12 +3,21 @@
 #define SERVOHANDLER_H
 
 #include <Arduino.h>
-
+#include <ServoHandler.h>
 class ServoHandler
 {
 public:
-    ServoHandler(){}
+    ServoHandler(uint8_t pin); // Standartconstructor for standart degress
+    ServoHandler(uint8_t pin, uint8_t startDegress, uint8_t endDegrees);
+    void setDegrees(uint8_t degrees);
+    uint8_t getDegrees();
+    void updateDegrees(); // ask if the 
+
 private:
+    uint8_t startDegress_; 
+    uint8_t endDegrees_;
+
+    uint8_t currentDegress_;
 };
 
 #endif // SERVOHANDLER_H
