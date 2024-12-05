aba81c0cf3df1a7a8a69c81057fc605361650498
Lukas <traktor.koeppl@gmail.com>
2024-12-05 13:19:19 +0100

    create example

--- example/main.cpp
+++ example/main.cpp
@@ -1,11 +1,20 @@
 #include <Arduino.h>
 #include <ServoHandler.h>
 
+#define PIN 8
+
+ServoHandler Servo(PIN);
 
 void setup() {
   Serial.begin(115200);
+
 }
 
 void loop() {
-
+  for (int i = 0; i < 180; i++)
+  {
+    Servo.setDegrees(i);
+    Servo.updateDegrees();
+    delay(100);
+  }
 }
