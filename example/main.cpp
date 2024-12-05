a3336e043ec5b871cc2d530563e9e85885c62593
deutzfahrer-luki <traktor.koeppl@gmail.com>
2024-12-05 22:58:46 +0100

    make not finish example, but for the debugging of the main lib logik code usefull

--- example/main.cpp
+++ example/main.cpp
@@ -1,20 +1,35 @@
 #include <Arduino.h>
 #include <ServoHandler.h>
 
-#define PIN 8
+#define PIN 33
+
+int i =0;
+
+unsigned long long currentTime = 0;
+unsigned long long oldTime = currentTime;
 
 ServoHandler Servo(PIN);
 
 void setup() {
   Serial.begin(115200);
+  Servo.setDegrees(120);
+  Serial.println("HALLO");
+  Serial.println(Servo.calcDutyCycle(120));
 
 }
 
 void loop() {
-  for (int i = 0; i < 180; i++)
-  {
-    Servo.setDegrees(i);
-    Servo.updateDegrees();
-    delay(100);
-  }
+  Servo.updateDegrees();
+  // currentTime = millis();
+  // if (currentTime > oldTime+100)
+  // {
+  //   Servo.setDegrees(i);
+  //   if (i > 160)
+  //     i =0;
+  //   i = i + 20;
+  //   Serial.println(i);
+  //   oldTime = currentTime;
+  // }
+  // Serial.println(Servo.calcDutyCycle(90));
+  // delay(10000);
 }
